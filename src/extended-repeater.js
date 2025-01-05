7047faa827e673fb0ebe91f8284a9da868555299
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-01-05 13:19:42 +0300

    feat:solcve task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  options.addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+
+  let additionPart = '';
+  if (options.additionRepeatTimes) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      additionPart += options.addition;
+      if (i < options.additionRepeatTimes - 1) {
+        additionPart += options.additionSeparator || '|';
+      }
+    }
+  } else {
+    additionPart = options.addition;
+  }
+
+  let result = '';
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str + additionPart;
+      if (i < options.repeatTimes - 1) {
+        result += options.separator || '+';
+      }
+    }
+  } else {
+    result = str + additionPart;
+  }
+
+  return result;
 }
 
 module.exports = {
